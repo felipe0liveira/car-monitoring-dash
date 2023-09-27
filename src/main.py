3a12203070ce8e7b5c96ef2bc8bb7f5fe0b0fbc9
Felipe Oliveira <felpd3v@gmail.com>
2023-09-27 09:42:01 -0300

    feat: show display views

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ import machine
 import random
 import time
 
+# Constants
+LAMBDA_TARGET_LIMIT = 0.97
+OIL_PRESSURE_TARGET_LIMIT = 1
+FUEL_PRESSURE_TARGET_LIMIT = 3
+
 # Display Settings
 WIDTH = 128
 HEIGHT = 64
@@ -13,9 +18,23 @@ oled = ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 digit_initial_x_px = [0, 0, 52, 93]
 
+digit_y_start = 0
 digit_max_width = 35
 digit_max_height = 64
 digit_thicknes = 10
+
+# Led Settings
+led_pin = 13
+led = machine.Pin(led_pin, machine.Pin.OUT)
+
+# Button Settings
+button_pin = 24
+button = machine.Pin(button_pin, machine.Pin.IN)
+
+# Display Mode
+DISPLAY_MODES = ["COMBINED", "LAMBDA", "OIL PRESSURE", "FUEL PRESSURE"]
+
+default_value_format = "{0:.2f}"
 
 
 def draw_single_number(num, position=1, color=1):
@@ -117,8 +136,7 @@ def draw_single_number(num, position=1, color=1):
 
 
 def draw_float(float_num=0.00, color=1):
-    my_formatter = "{0:.2f}"
-    output = my_formatter.format(float_num)
+    output = default_value_format.format(float_num)
 
     num1 = output[0]
     num2 = output[2]
@@ -131,27 +149,105 @@ def draw_float(float_num=0.00, color=1):
     draw_single_number(num3, 3, color)
 
 
+def get_lambda_data():
+    second = random.randint(60, 99)
+    return default_value_format.format(float("0." + str(second)))
+
+
+def get_oil_pressure_data():
+    first = random.randint(6, 9)
+    second = random.randint(60, 99)
+    return default_value_format.format(float(str(first) + "." + str(second)))
+
+
+def get_fuel_pressure_data():
+    first = random.randint(1, 9)
+    second = random.randint(60, 99)
+    return default_value_format.format(float(str(first) + "." + str(second)))
+
+
+def centered_x(text):
+    return int((WIDTH/2) - (len(text)/2*8))
+
+
 def main():
+    display_selector = 0
+
     oled.fill(1)
     first_line = "SANJATUNING"
     second_line = "Monitor v1.0"
-    oled.text(first_line, int((WIDTH/2) - (len(first_line)/2*8)), 22, 0)
-    oled.text(second_line, int((WIDTH/2) - (len(second_line)/2*8)), 32, 0)
+    oled.text(first_line, centered_x(first_line), 22, 0)
+    oled.text(second_line, centered_x(second_line), 32, 0)
 
     oled.show()
-    time.sleep(3)
+    time.sleep(1)
 
     while True:
-        second = random.randint(60, 99)
-        float_number = float("0." + str(second))
-        oled.fill(1 if float_number > 0.97 else 0)
-        draw_float(float_number, 0 if float_number > 0.97 else 1)
+        print(display_selector)
+        print(DISPLAY_MODES[display_selector])
 
-        if float_number > 0.97:
-            oled.rect(0, 0, 128, 64, 1)
+        if button.value() == 1:
+            next_display_selector = display_selector + 1
+
+            if next_display_selector == len(DISPLAY_MODES):
+                next_display_selector = 0
+
+            display_selector = next_display_selector
+
+            current_title = DISPLAY_MODES[display_selector]
+            oled.fill(1)
+            oled.text(current_title, centered_x(current_title), 28, 0)
+            oled.show()
+
+            time.sleep(1)
+
+        oled.fill(0)
+
+        if display_selector == 0:
+            lambda_value = get_lambda_data()
+            oled.text("Wideband", 0, 0, 1)
+            oled.text(lambda_value, WIDTH - (len(lambda_value)*8), 0, 1)
+
+            oil_pressure_value = get_oil_pressure_data()
+            oled.text("Oil", 0, 28, 1)
+            oled.text(oil_pressure_value, WIDTH -
+                      (len(oil_pressure_value)*8), 28, 1)
+
+            fuel_pressure_value = get_fuel_pressure_data()
+            oled.text("Fuel", 0, 56, 1)
+            oled.text(fuel_pressure_value, WIDTH -
+                      (len(fuel_pressure_value)*8), 56, 1)
+
+        if display_selector == 1:
+            lambda_value = get_lambda_data()
+            draw_float(float(lambda_value), 1)
+
+            led.off()
+
+            if float(lambda_value) > LAMBDA_TARGET_LIMIT:
+                led.on()
+
+        if display_selector == 2:
+            oil_pressure_value = get_oil_pressure_data()
+            draw_float(float(oil_pressure_value), 1)
+
+            led.off()
+
+            if float(oil_pressure_value) < OIL_PRESSURE_TARGET_LIMIT:
+                led.on()
+
+        if display_selector == 3:
+            fuel_pressure_value = get_fuel_pressure_data()
+            draw_float(float(fuel_pressure_value), 1)
+
+            led.off()
+
+            if float(fuel_pressure_value) < OIL_PRESSURE_TARGET_LIMIT:
+                led.on()
+
+        time.sleep_ms(100)
 
         oled.show()
-        time.sleep_ms(100 if float_number <= 0.97 else 50)
 
 
 main()
